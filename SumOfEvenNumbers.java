f14a3c9ff386bdd89ab9a200fd40b34eda804f2f
Anu-Alias2024 <alias.aanu@gmail.com>
2024-08-16 20:21:39 +0530

    first commit

---
+++ SumOfEvenNumbers.java
@@ -0,0 +1,14 @@
+package com.ust.gitrepository;
+
+import java.util.Scanner;
+
+public class SumOfEvenNumbers {
+
+	public static void main(String[] args) {
+		try(Scanner sc = new Scanner(System.in)) {
+			System.out.println("enter the limit");
+			int n =sc.nextInt();
+		}
+	}
+
+}
