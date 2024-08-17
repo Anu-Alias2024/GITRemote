85b4973d30195056e8fa2eac42bb9ad83e8896ce
Anu-Alias2024 <alias.aanu@gmail.com>
2024-08-17 07:35:35 +0530

    updated files

--- src/com/ust/gitrepository/SumOfEvenNumbers.java
+++ src/com/ust/gitrepository/SumOfEvenNumbers.java
@@ -1,5 +1,6 @@
 package com.ust.gitrepository;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SumOfEvenNumbers {
@@ -8,6 +9,19 @@ public class SumOfEvenNumbers {
 		try(Scanner sc = new Scanner(System.in)) {
 			System.out.println("enter the limit");
 			int n =sc.nextInt();
+			ArrayList<Integer> list =new ArrayList<Integer>();
+			for(int i=0;i<n;i++) {
+				list.add(sc.nextInt());
+			}
+			int sum=0;
+			for(int i : list) {
+				if(i%2==0) {
+					
+					sum +=i;
+					
+				}
+			}
+System.out.println(sum);
 		}
 	}
 
---
+++ src/com/ust/gitrepository/SumOfOddNumbers.java
@@ -0,0 +1,30 @@
+package com.ust.gitrepository;
+
+import java.util.Scanner;
+
+public class SumOfOddNumbers {
+
+		public static void main(String[] args) {
+			try(Scanner  sc = new Scanner(System.in)) {
+				System.out.println("enter the limit");
+				int n =sc.nextInt();
+			//	ArrayList<Integer> list =new ArrayList<Integer>();'
+				int [] list = new int[n];
+ 				for(int i=0;i<n;i++) {
+					//list.add(sc.nextInt());
+ 					list[i]=sc.nextInt();
+				}
+				int sum=0;
+				for(int i : list) {
+					if(i%2==1) {
+						
+						sum +=i;
+						
+					}
+				}
+	System.out.println(sum);
+			}
+		}
+
+	
+}
